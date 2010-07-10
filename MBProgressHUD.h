8b0baf2ee6ff65da368142b5d4a3e41df23e75c0
Fraser Speirs <fraser@speirs.org>
2010-07-10 08:58:19 +0100

    Defined some accessors to be nonatomic to fix compiler warnings.

--- MBProgressHUD.h
+++ MBProgressHUD.h
@@ -142,27 +142,27 @@ typedef enum {
  * MBProgressHUD operation mode. Switches between indeterminate (MBProgressHUDModeIndeterminate) and determinate
  * progress (MBProgressHUDModeDeterminate). The default is MBProgressHUDModeIndeterminate.
  */
-@property (assign) MBProgressHUDMode mode;
+@property (nonatomic, assign) MBProgressHUDMode mode;
 
 /** 
  * The HUD delegate object. If set the delegate will receive hudWasHidden callbacks when the HUD was hidden. The
  * delegate should conform to the MBProgressHUDDelegate protocol and implement the hudWasHidden method. The delegate
  * object will not be retained.
  */
-@property (assign) id<MBProgressHUDDelegate> delegate;
+@property (nonatomic, assign) id<MBProgressHUDDelegate> delegate;
 
 /** 
  * An optional short message to be displayed below the activity indicator. The HUD is automatically resized to fit
  * the entire text. If the text is too long it will get clipped by displaying "..." at the end. If left unchanged or
  * set to @"", then no message is displayed.
  */
-@property (copy) NSString *labelText;
+@property (nonatomic, copy) NSString *labelText;
 
 /** 
  * An optional details message displayed below the labelText message. This message is displayed only if the labelText
  * property is also set and is different from an empty string (@"").
  */
-@property (copy) NSString *detailsLabelText;
+@property (nonatomic, copy) NSString *detailsLabelText;
 
 /** 
  * The opacity of the HUD window. Defaults to 0.9 (90% opacity). 
@@ -221,7 +221,7 @@ typedef enum {
 /** 
  * The progress of the progress indicator, from 0.0 to 1.0. Defaults to 0.0. 
  */
-@property (assign) float progress;
+@property (nonatomic, assign) float progress;
 
 /** 
  * Display the HUD. You need to make sure that the main thread completes its run loop soon after this method call so
